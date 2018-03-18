52c4fa4ea94a75b9b81692f2bc6aa78265ae9501
EmanMacario <emacario1997@hotmail.com>
2018-03-18 21:27:43 +1100

    Added new Board and Piece classes

---
+++ piece.py
@@ -0,0 +1,15 @@
+'''
+This file contains the Piece class,
+representing a standard black or white
+piece in the game 'Watch Your Back!'
+'''
+
+class Piece:
+    def __init__(self, char):
+        self.char = char
+
+
+
+
+
+    
