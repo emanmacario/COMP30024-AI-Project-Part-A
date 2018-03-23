9da4fff0de8a5b78e88798a43508abfdcbfe9a12
EmanMacario <emacario1997@hotmail.com>
2018-03-23 21:49:55 +1100

    Parsed the input and filled the board with squares, and sets neighbours

--- board.py
+++ board.py
@@ -3,59 +3,157 @@ This file contains the Board class,
 which represents the entire game board.
 '''
 
+from square import *
+
+
 class Board:
     """
-    A 'Watch Your Back!' game board, used for
+    A game board for 'Watch Your Back!', used for
     storing the state of the game, and trying
     out different scenarios.
     """
 
-    # Class variables
-    WIDTH = 8
-    HEIGHT = 8
+    # Possible directions of movement
+    DIRECTION_UP    = "UP"
+    DIRECTION_DOWN  = "DOWN"
+    DIRECTION_LEFT  = "LEFT"
+    DIRECTION_RIGHT = "RIGHT"
+
+    # Possible pieces
+    EMPTY_SQUARE  = '-'
+    CORNER_SQUARE = 'X'
+    BLACK_SQUARE  = '@'
+    WHITE_SQUARE  = 'O'
 
 
-    def __init__(self, width, height):
+    def __init__(self, board_config=None, width=8, height=8):
         """
         Board constructor. Takes as input the width
         and height of the game board.
         """
         self.width = width
         self.height = height
-        self.grid = [[] for _ in range(WIDTH)]
-        self.curr_rows = 0
+        self.squares = [[] for _ in range(width)]
+
+        if (board_config):
+            self.fill_board(board_config)
+
+        self.set_all_neighbours()
+
+        # Print the board configuration
+        print(self)
+
+                
+
+    def fill_board(self, board_config):
+        """Fills in the board with squares"""
+
+        for row in range(self.height):
+            for column in range(self.width):
+                char = board_config[row][column]
+                self.squares[row].append(Square(row, column, char))
 
 
 
-    def total_pieces(self):
+    def set_neighbours(self, row, column):
+        """Sets the neighbours for a given square"""
+        if row != 0:
+            self.squares[row][column].set_neighbour(Board.DIRECTION_UP,
+                self.squares[row-1][column])
+
+        if column != 0:
+            self.squares[row][column].set_neighbour(Board.DIRECTION_LEFT,
+                self.squares[row][column-1])
+
+        if row != self.width-1:
+            self.squares[row][column].set_neighbour(Board.DIRECTION_DOWN,
+                self.squares[row+1][column])
+
+        if column != self.height-1:
+            self.squares[row][column].set_neighbour(Board.DIRECTION_RIGHT,
+                self.squares[row][column+1])
+
+
+
+    def set_all_neighbours(self):
+        """Sets the neighbours for all squares on the board"""
+        for row in range(self.height):
+            for column in range(self.width):
+                self.set_neighbours(row, column)
+
+
+
+    def make_move(self, row, column):
         """
-        Counts the total number of pieces
-        on the board.
-        """
-        pass
-
-
-    def add_row(self, row):
-        """
-        Adds a row to the board if it is possible.
-        """
-        grid[rows] = row
-        row += 1
-
-
-
-    @classmethod
-    def from_str(cls, row_str):
-        """
-        Takes a string representing one row
-        of the game board, and parses it into a list
-        representation.
+        Makes a valid move for a given piece.
         """
 
-        # Obtain a new row
-        row = row_str.rstrip('\r').split(' ')
 
-        return row
+    def is_valid_move(self, direction, row, column):
+        """
+        Returns True if a move is valid, else returns False.
+        The row and column number is of the piece to be moved,
+        and the direction indicates the direction to be moved.
+        """
 
+        # Get the originating square and possible piece
+        square = self.squares[row][column]
+        piece = square.char
+
+        # First check that the certain piece on a square 
+        # we are trying to move actually exists.
+        if piece == self.EMPTY_SQUARE or piece == self.CORNER_SQUARE:
+            return False
+
+        # Calculate the new co-ordinates of the piece, given the direction
+        if direction == self.DIRECTION_DOWN:
+            row += 1
+        elif direction == self.DIRECTION_UP:
+            row -= 1
+        elif direction == self.DIRECTION_LEFT:
+            column -= 1
+        else:
+            column += 1
+
+        # Check if the move is inside the bounds of the board
+        if row < 0 or row > self.height-1 or column < 0 or column > self.width-1:
+            return False
+
+        # If the square is empty, it is a valid move
+        if squares[row][column].char == self.EMPTY_SQUARE:
+            return True
+
+
+        # Otherwise, check if we can perform a 'jump'
+        if direction == self.DIRECTION_DOWN:
+            row += 1
+        elif direction == self.DIRECTION_UP:
+            row -= 1
+        elif direction == self.DIRECTION_LEFT:
+            column -= 1
+        else:
+            column += 1
+
+        # Check if the move is inside the bounds of the board
+        if row < 0 or row > self.height-1 or column < 0 or column > self.width-1:
+            return False
+
+
+        # If the square is empty, it is a valid move
+        if squares[row][column].char == self.EMPTY_SQUARE:
+            return True
+
+
+
+    def __repr__(self):
+        """Representation of the current board"""
+        board_repr = ""
+
+        for row in self.squares:
+            for square in row:
+                board_repr += square.__repr__()
+            board_repr += "\n"
+
+        return board_repr
 
 
