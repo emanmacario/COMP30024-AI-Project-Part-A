7dfdbe0cb07f3ba3b9595326864bb34c63ea7641
EmanMacario <emacario1997@hotmail.com>
2018-03-29 20:46:41 +1100

    Optimised search by the tiniest amount, by not reversing paths

--- search.py
+++ search.py
@@ -12,7 +12,11 @@ import sys
 class Node:
 
     """
-    A node in a search tree
+    A node in a search tree. Contains a pointer to the parent (the node
+    that this is a successor of) and to the actual state for this node. Note
+    that if a state is arrived at by two paths, then there are two nodes with
+    the same state.  Also includes the action that got us to this state, and
+    the total path_cost (also known as g) to reach the node.
     """
 
     def __init__(self, state, parent=None, action=None, path_cost=None):
@@ -52,15 +56,22 @@ class Node:
 
 
 
-    def path(self):
-        """Return a list of nodes forming the path from the root to this node"""
+    def path(self, reverse=True):
+        """
+        Return a list of nodes forming the path from the root to this node.
+        If the reversed flag is set to False, gets the path from node to root
+        instead.
+        """
 
         node, path_back = self, []
         while node:
             path_back.append(node)
             node = node.parent
 
-        return list(reversed(path_back))
+        if reverse:
+            return list(reversed(path_back))
+
+        return path_back
 
 
     def __eq__(self, other):
@@ -88,7 +99,7 @@ def depth_limited_search(game, limit):
         else:
     
             cutoff_occurred = False
-            path = node.path()
+            path = node.path(reverse=False)
 
             for child in node.expand(game):
 
@@ -112,7 +123,8 @@ def depth_limited_search(game, limit):
 def iterative_deepening_search(game):
     """
     Perform iterative deepening search on
-    a state space for the game Watch Your Back!
+    a state space for a given instance of
+    the game 'Watch Your Back!'
     """
     for depth in range(sys.maxsize):
         result = depth_limited_search(game, depth)
